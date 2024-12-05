b8b53b327fdf5f1f0acf8cda279a85761aa39970
sarvesh-0 <sarveshteware651@gmail.com>
2024-12-05 15:31:09 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { CarComponent } from './car/car.component';
+import { BusComponent } from './bus/bus.component';
+import { TrainComponent } from './train/train.component';
+import { FlightComponent } from './flight/flight.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomeComponent },
+    { path: 'hotel', component: HotelComponent },
+    { path: 'car', component: CarComponent },
+    { path: 'bus', component: BusComponent },
+    { path: 'train', component: TrainComponent },
+    { path: 'flight', component: FlightComponent }];
