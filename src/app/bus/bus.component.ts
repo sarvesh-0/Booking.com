3245a478547b091d1025dfcaaf11dada813c35ba
sarvesh-0 <sarveshteware651@gmail.com>
2024-12-07 14:16:56 +0530

    commit

--- src/app/bus/bus.component.ts
+++ src/app/bus/bus.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-bus',
-  imports: [],
+  imports: [MatButtonModule,MatCardModule,MatToolbarModule,MatIconModule],
   templateUrl: './bus.component.html',
   styleUrl: './bus.component.css'
 })
